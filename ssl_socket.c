3340a86fee2a7cc81ed9c754e5b0bd22ce17eeaf
Amarto <aar2160@columbia.edu>
2015-11-08 17:27:38 -0500

    Reimplemented HandleClient

--- ssl_socket.c
+++ ssl_socket.c
@@ -27,10 +27,110 @@ void shutdown_SSL()
     SSL_free(ssl);
 }
 
-int sockfd, newsockfd;
-SSL_CTX *sslctx;
-SSL *cSSL;
 
-initialize_SSL();
 
-sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+void HandleClient(int clntSocket)
+{
+    char echoBuffer[RCVBUFSIZE];        /* Buffer for echo string */
+    int recvMsgSize;                    /* Size of received message */
+
+    /* Receive message from client */
+    if ((recvMsgSize = recv(clntSocket, echoBuffer, RCVBUFSIZE, 0)) < 0)
+        DieWithError("recv() failed");
+
+    /* Send received string and receive again until end of transmission */
+    while (recvMsgSize > 0)      /* zero indicates end of transmission */
+    {
+        /* Echo message back to client */
+        if (SSL_write(cSSL, "Hi :3\n", 6); != recvMsgSize)
+            DieWithError("send() failed");
+
+        /* See if there is more data to receive */
+        if ((recvMsgSize = SSL_read(cSSL, (char *)charBuffer, nBytesToRead)) < 0)
+            DieWithError("recv() failed");
+    }
+
+    close(clntSocket);    /* Close client socket */
+}
+
+int main(int argc, char *argv[])
+{
+    int servSock;                    /* Socket descriptor for server */
+    int clntSock;                    /* Socket descriptor for client */
+    struct sockaddr_in echoServAddr; /* Local address */
+    struct sockaddr_in echoClntAddr; /* Client address */
+    unsigned short echoServPort;     /* Server port */
+    unsigned int clntLen;            /* Length of client address data structure */
+
+    if (argc != 2)     /* Test for correct number of arguments */
+    {
+        fprintf(stderr, "Usage:  %s <Server Port>\n", argv[0]);
+        exit(1);
+    }
+
+    echoServPort = atoi(argv[1]);  /* First arg:  local port */
+
+    SSL_CTX *sslctx;
+	SSL *cSSL;
+
+
+	initialize_SSL();
+
+
+    /* Create socket for incoming connections */
+    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        DieWithError("socket() failed");
+      
+    /* Construct local address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));   /* Zero out structure */
+    echoServAddr.sin_family = AF_INET;                /* Internet address family */
+    echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* Any incoming interface */
+    echoServAddr.sin_port = htons(echoServPort);      /* Local port */
+
+    /* Bind to the local address */
+    if (bind(servSock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+        DieWithError("bind() failed");
+
+    /* Mark the socket so it will listen for incoming connections */
+    if (listen(servSock, MAXPENDING) < 0)
+        DieWithError("listen() failed");
+
+    for (;;) /* Run forever */
+    {
+        /* Set the size of the in-out parameter */
+        clntLen = sizeof(echoClntAddr);
+
+        /* Wait for a client to connect */
+        if ((clntSock = accept(servSock, (struct sockaddr *) &echoClntAddr, 
+                               &clntLen)) < 0)
+            DieWithError("accept() failed");
+
+
+    sslctx = SSL_CTX_new( SSLv23_server_method());
+	SSL_CTX_set_options(sslctx, SSL_OP_SINGLE_DH_USE);
+	int use_cert = SSL_CTX_use_certificate_file(sslctx, "/serverCertificate.pem" , SSL_FILETYPE_PEM);
+
+	int use_prv = SSL_CTX_use_PrivateKey_file(sslctx, "/serverCertificate.pem", SSL_FILETYPE_PEM);
+
+	cSSL = SSL_new(sslctx);
+	SSL_set_fd(cSSL, newsockfd );
+	//Here is the SSL Accept portion.  Now all reads and writes must use SSL
+	ssl_err = SSL_accept(cSSL);
+
+	if(ssl_err <= 0)
+	{
+	    //Error occurred, log and close down ssl
+	    ShutdownSSL();
+	}
+
+        /* clntSock is connected to a client! */
+
+        printf("Handling client %s\n", inet_ntoa(echoClntAddr.sin_addr));
+
+        HandleClient(clntSock);
+    }
+    /* NOT REACHED */
+}
+
+
